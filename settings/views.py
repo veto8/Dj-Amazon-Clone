9efadcd69d7704ffb5c1b47afe6490b6e4026cea
Omarmoatz <omar.raouf.002@gmail.com>
2023-11-29 20:30:05 +0200

    edit prodcuts page

--- settings/views.py
+++ settings/views.py
@@ -6,12 +6,14 @@ from products.models import Products,Brand,Category,ProductReviews
 def home(request):
     sale_product = Products.objects.filter(flag='Sale')[:10]
     feature_product = Products.objects.filter(flag='Feature')[0:6]
-    new_product = Products.objects.filter(flag='New')[0:15]
+    new_product = Products.objects.filter(flag='New')[0:10]
+    product = Products.objects.filter(flag='New')[20:30]
     review = ProductReviews.objects.all()
     brand = Brand.objects.all()
     category = Category.objects.all().annotate(product_num=Count('Category'))
 
     return render(request,'settings/home.html',{
+        'product':product,
         'product_sale':sale_product,
         'products_fea':feature_product,
         'product_new':new_product,
