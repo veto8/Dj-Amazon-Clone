f81e6c7413ca3cd7139d95d7b4d17052c7388082
Omarmoatz <omar.raouf.002@gmail.com>
2023-12-02 02:26:55 +0200

    show order details in orders page & order status & pagination

--- orders/views.py
+++ orders/views.py
@@ -6,3 +6,8 @@ from .models import Order ,OrderDetail,Cart,CartDetail
 
 class OrderList(generic.ListView):
     model = Order
+    paginate_by = 4
+
+    def get_queryset(self):
+        return super().get_queryset().filter(user=self.request.user)
+    
