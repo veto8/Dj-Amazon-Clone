a1e55a8ca7aecbd4c6fb158fe78c613a53b610e3
Omarmoatz <omar.raouf.002@gmail.com>
2023-12-13 17:28:49 +0200

    fix (cash) erorr

--- settings/views.py
+++ settings/views.py
@@ -5,7 +5,7 @@ from products.models import Products,Brand,Category,ProductReviews
 from django.views.decorators.cache import cache_page
 
 
-# @cache_page(60 * 1)
+@cache_page(60 * 1)
 def home(request):
     sale_product = Products.objects.filter(flag='Sale')[:10]
     feature_product = Products.objects.filter(flag='Feature')[0:6]
