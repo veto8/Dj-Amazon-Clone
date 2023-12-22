427b86907687f7b51433fdf7ede636a1091bb36c
Omarmoatz <omar.raouf.002@gmail.com>
2023-12-22 13:41:55 +0200

    design account models

--- acounts/models.py
+++ acounts/models.py
@@ -1,38 +1,45 @@
-# from django.db import models
-# from django.contrib.auth.models import User
-# from django.utils.translation import gettext as _
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import gettext as _
+from utiles.generate_code import genrate_code
 
 
 
-# class Profile(models.Model):
-#     user =models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-#     email = models.EmailField(_("Email"), max_length=200)
-#     image = models.ImageField(_("Image"), upload_to='profile/')
+class Profile(models.Model):
+    user =models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    email = models.EmailField(_("Email"), max_length=200)
+    image = models.ImageField(_("Image"), upload_to='profile/')
+    code = models.CharField(_("Code"), max_length=50, default=genrate_code)
 
-# def __str__(self):
-#     return self.email
-
-
-# PHONE_TITLE =(
-#     ('Secondary','Secondary'),
-#     ('Primary','Primary'),
-# )
-# class PhoneNum(models.Model):
-#     user =models.OneToOneField(User, on_delete=models.CASCADE, related_name='user_phone')
-#     title = models.CharField(_("Title"), max_length=50, choices=PHONE_TITLE)
-#     number = models.PositiveIntegerField(_("Number"))
+def __str__(self):
+    return self.email
 
 
+PHONE_TITLE =(
+    ('Secondary','Secondary'),
+    ('Primary','Primary'),
+)
+class PhoneNum(models.Model):
+    user =models.OneToOneField(User, on_delete=models.CASCADE, related_name='user_phone')
+    title = models.CharField(_("Title"), max_length=50, choices=PHONE_TITLE)
+    number = models.PositiveIntegerField(_("Number"))
 
-# ADDRESS_TITLE =(
-#     ('Home','Home'),
-#     ('Office','Office'),
-#     ('Busseniss','Busseniss'),
-#     ('Academy','Academy'),
-#     ('Other','Other'),
-# )
-# class Address(models.Model):
-#     user =models.OneToOneField(User, on_delete=models.CASCADE, related_name='user_address')
-#     title = models.CharField(_("Title"), max_length=50, choices=ADDRESS_TITLE)
-#     address = models.PositiveIntegerField(_("Number"))
-#     notes = models.TextField(_("Notes"),blank=True, null=True)
+def __str__(self):
+    return self.user
+
+
+ADDRESS_TITLE =(
+    ('Home','Home'),
+    ('Office','Office'),
+    ('Busseniss','Busseniss'),
+    ('Academy','Academy'),
+    ('Other','Other'),
+)
+class Address(models.Model):
+    user =models.OneToOneField(User, on_delete=models.CASCADE, related_name='user_address')
+    title = models.CharField(_("Title"), max_length=50, choices=ADDRESS_TITLE)
+    address = models.PositiveIntegerField(_("Number"))
+    notes = models.TextField(_("Notes"),blank=True, null=True)
+
+def __str__(self):
+    return self.user
