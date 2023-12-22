1ef8ed738ceaa8a28c45c89fa20c334310554a0f
Omarmoatz <omar.raouf.002@gmail.com>
2023-12-22 14:13:10 +0200

    add create profile signal

--- acounts/models.py
+++ acounts/models.py
@@ -1,18 +1,28 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.translation import gettext as _
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 from utiles.generate_code import genrate_code
 
 
 
 class Profile(models.Model):
     user =models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    email = models.EmailField(_("Email"), max_length=200)
-    image = models.ImageField(_("Image"), upload_to='profile/')
+    email = models.EmailField(_("Email"), max_length=200 ,blank=True, null=True)
+    image = models.ImageField(_("Image"), upload_to='profile/' ,blank=True, null=True)
     code = models.CharField(_("Code"), max_length=50, default=genrate_code)
 
-def __str__(self):
-    return self.email
+    def __str__(self):
+        return self.email
+@receiver(post_save,sender=User)
+def create_profile(sender,instance,created,**kwargs):
+    if created:
+        Profile.objects.create(
+            user = instance
+        )
+
 
 
 PHONE_TITLE =(
@@ -24,8 +34,10 @@ class PhoneNum(models.Model):
     title = models.CharField(_("Title"), max_length=50, choices=PHONE_TITLE)
     number = models.PositiveIntegerField(_("Number"))
 
-def __str__(self):
-    return self.user
+    def __str__(self):
+        return self.user
+
+
 
 
 ADDRESS_TITLE =(
@@ -41,5 +53,5 @@ class Address(models.Model):
     address = models.PositiveIntegerField(_("Number"))
     notes = models.TextField(_("Notes"),blank=True, null=True)
 
-def __str__(self):
-    return self.user
+    def __str__(self):
+        return self.user
